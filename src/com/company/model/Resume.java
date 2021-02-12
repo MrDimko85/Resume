6a8a8e72c53451480d6cbec446fd547876aef63f
MrDimko85 <mr.dimko85@gmail.com>
2021-02-12 10:55:54 +0300

    initial commit

---
+++ src/com/company/model/Resume.java
@@ -0,0 +1,18 @@
+package com.company.model;
+
+public class Resume {
+    String uuid;
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+
+    @Override
+    public String toString() {
+        return uuid;
+    }
+}
---
+++ src/com/company/storage/ArrayStorage.java
@@ -0,0 +1,52 @@
+package com.company.storage;
+
+import com.company.model.Resume;
+
+public class ArrayStorage {
+    private Resume[] storage = new Resume[10000];
+    private int size = 0;
+
+    public void clear (){
+        for (int i = 0; i < size; i++) {
+            storage[i] = null;
+        }
+        size = 0;
+    }
+
+    public void save(Resume r) {
+        storage[size] = r;
+        size++;
+    }
+
+    public Resume get(String uuid){
+        for (int i = 0; i < size; i++) {
+            if (uuid.equals(storage[i].getUuid())) {
+                return storage[i];
+            }
+        }
+        return null;
+    }
+
+    public void delete(String uuid) {
+        for (int i = 0; i < size; i++) {
+            if (uuid.equals(storage[i].getUuid())) {
+                storage[i] = storage[size - 1];
+                storage[size - 1] = null;
+                size--;
+                }
+            }
+        }
+
+
+   public Resume[] getAll() {
+        Resume[] result = new Resume[size];
+       for (int i = 0; i < size; i++) {
+           result[i] = storage[i];
+       }
+       return result;
+    }
+
+    public int size() {
+        return size;
+    }
+}
